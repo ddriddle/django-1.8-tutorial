79d639cafb20d1979e182c885e2db88df1583404
David D. Riddle <ddriddle@illinois.edu>
2015-05-06 14:24:19 -0500

    Playing with the API -- add custom method
    Edit models.py: Add was_published_recently
    
    https://docs.djangoproject.com/en/1.8/intro/tutorial01/#playing-with-the-api

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -7,6 +10,9 @@ class Question(models.Model):
 
     def __unicode__(self):              # __str__ on Python 3
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
