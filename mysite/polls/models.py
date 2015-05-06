3e342f85392aaa75f917e1b288b298573636c8c7
David D. Riddle <ddriddle@illinois.edu>
2015-05-06 14:21:25 -0500

    Playing with the API -- add __unicode__
    Edit models.py
    
    https://docs.djangoproject.com/en/1.8/intro/tutorial01/#playing-with-the-api

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,8 +5,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __unicode__(self):              # __str__ on Python 3
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __unicode__(self):              # __str__on Python 3
+        return self.choice_text
